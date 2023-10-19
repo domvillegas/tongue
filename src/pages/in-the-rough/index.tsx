650f01ab8d1225788d0925b77ece84e6f9e1f9d5
domvillegas <domvillgithub@gmail.com>
2023-10-18 17:09:11 -0700

    Add InTheRough and Upcoming copy.

--- src/pages/in-the-rough/index.tsx
+++ src/pages/in-the-rough/index.tsx
@@ -1,11 +1,54 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MadeWithLove from "@/components/MadeWithLove/MadeWithLove";
+import MenuButton from "@/components/MenuButton/MenuButton";
+import styles from "./InTheRough.module.scss";
 
 const InTheRough = () => {
+  useEffect(() => {
+    const body = document.getElementsByTagName("body")[0];
+    body.classList.add("fadeIn");
+
+    setTimeout(() => {
+      body.classList.remove("fadeOut");
+      body.classList.remove("fadeIn");
+    }, 500);
+  });
+
   return (
-    <div>
-      <h1>In the Rough</h1>
-      <MadeWithLove />
+    <div className={styles.inTheRough}>
+      <MenuButton />
+      <div className={styles.contentContainer}>
+        <h2>In the Rough</h2>
+        <p>
+          In The Rough (ITR) is a works in progress showing for movement based
+          artists to present new ideas, receive feedback from a live audience
+          and peers, and to establish and further strengthen connections between
+          movers in the Portland area. ITR was first produced at FLOOR Center
+          for Dance in 2019 and the platform produced three successful
+          iterations, which provided performance opportunities for over 25
+          Portland based artists. ITR is a program that serves emerging artists,
+          improvisers, and seasoned choreographers and allows creators to put
+          their work in front of an audience in an intimate, informal setting.{" "}
+        </p>
+        <div className={styles.timePlaceContainer}>
+          <span>WHEN: February 3, 2024</span>
+          <span>
+            WHERE:{" "}
+            <a href="https://newexpressiveworks.org/" target="_blank">
+              New Expressive Works
+            </a>
+          </span>
+        </div>
+        <a
+          className={styles.button}
+          href="https://docs.google.com/forms/d/e/1FAIpQLScP-nH8MsVoa-eX9myx9-w5wYnWvGKoeRhbM7ZKHB5-EyQYPA/viewform?usp=sf_link"
+          target="_blank"
+        >
+          Apply Here
+        </a>
+        <span>Applications due December 1, 2023</span>
+      </div>
+      <MadeWithLove className={styles.madeWithLove} />
     </div>
   );
 };
--- src/pages/upcoming/index.tsx
+++ src/pages/upcoming/index.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MadeWithLove from "@/components/MadeWithLove/MadeWithLove";
+import MenuButton from "@/components/MenuButton/MenuButton";
+import styles from "./Upcoming.module.scss";
 
 const Upcoming = () => {
+  useEffect(() => {
+    const body = document.getElementsByTagName("body")[0];
+    body.classList.add("fadeIn");
+
+    setTimeout(() => {
+      body.classList.remove("fadeOut");
+      body.classList.remove("fadeIn");
+    }, 500);
+  });
+
   return (
-    <div>
-      <h1>Upcoming</h1>
-      <MadeWithLove />
+    <div className={styles.upcoming}>
+      <MenuButton />
+      <div className={styles.contentContainer}>
+        <span>
+          Weekly <u>Free</u> Company Class
+        </span>
+        <span>Tuesdays 6-7:15pm</span>
+        <a href="https://newexpressiveworks.org/" target="_blank">
+          New Expressive Works
+        </a>
+        <span>810 SE Belmont St, Portland, OR 97214</span>
+      </div>
+      <MadeWithLove className={styles.madeWithLove} />
     </div>
   );
 };
