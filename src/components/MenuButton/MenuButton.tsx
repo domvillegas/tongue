c818a39d193cfcde73cd6762799cbc37f86ebe6a
domvillegas <domvillgithub@gmail.com>
2023-12-08 16:18:43 -0800

    Add new flyer. Add new tongue logo.

--- src/components/MenuButton/MenuButton.tsx
+++ src/components/MenuButton/MenuButton.tsx
@@ -3,6 +3,7 @@ import styles from "./MenuButton.module.scss";
 import { pushRouteWithFade } from "@/utils/helpers";
 import { useRouter } from "next/router";
 import yinyang from '../../assets/backgrounds/yinyang.png';
+import tongueLogo from '../../assets/backgrounds/tongue-logo.png';
 import Image from "next/image";
 
 const MenuButton = () => {
@@ -14,7 +15,7 @@ const MenuButton = () => {
 
   return (
     <div className={styles.menuButton} onClick={clickHandler}>
-        <Image src={yinyang.src} alt="yinyang" width={50} height={50} />
+        <Image src={tongueLogo.src} alt="yinyang" width={50} height={50} />
     </div>
   );
 };
